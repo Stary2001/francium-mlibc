ArsenArsen/mlibc

options/linux: implement xattr.h

sysdeps/aero/include/abi-bits/xattr.h
@@ -1,0 +1,1 @@
+../../../../abis/mlibc/xattr.h

Stary2001/francium-mlibc

sysdeps/aero/include/abi-bits/xattr.h
@@ -1,0 +1,1 @@
+../../../../abis/linux/xattr.h